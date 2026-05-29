b14a1362910972383667bb43128b2e7d70cd7ffd
[dyad] <harchit23@gmail.com>
2026-05-28 23:40:05 -0400

    [dyad] wrote 1 file(s)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,7 +49,7 @@ const Navbar = () => {
             <span className="font-brand font-normal text-xl leading-none text-white">
               Asha
             </span>
-            <span className="font-['Playfair_Display_Italic'] italic font-normal text-yellow-400 text-sm tracking-wider">
+            <span className="font-['Playfair_Display_Italic'] italic font-normal text-yellow-400 text-base tracking-wider">
               Interiors
             </span>
           </div>
